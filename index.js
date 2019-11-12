cb7776ff272db417e08ad6334a8d8c4c87275726
gaolu11 <768065158@qq.com>
2019-11-12 11:32:05 +0800

    fix: update package

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+const SVGParser = require('convertpath')
 const Parse = require('./lib/index.js')
 // eslint-disable-next-line no-unused-vars
 const { pathParse, serializePath } = require('svg-path-parse')
@@ -8,42 +10,79 @@ const { pathParse, serializePath } = require('svg-path-parse')
 // 贝塞尔曲线 cairo_curve_to()   cairo_rel_curve_to ()    => C/c、Q/q、T/t、S/s
 // cairo_close_path()   => Z/z
 
+const parse = SVGParser.parse('./calendar.svg', {
+  plugins: [
+    {
+      convertUseToGroup: true, // at first
+    },
+    {
+      convertShapeToPath: true,
+    },
+    {
+      removeGroups: true,
+    },
+    {
+      convertTransfromforPath: true,
+    },
+    {
+      viewBoxTransform: true, // at last
+    }
+  ],
+  size: 1000,
+})
+
+const paths = parse.getPathAttributes()
+const pathDatas = []
+paths.forEach(item => {
+  if (item.d) {
+    const { err, segments } = pathParse(item.d).relCairo({ round: 2 })
+    console.error('segments', segments)
+    if (!err) {
+      pathDatas.push({
+        'd': segments,
+        'attributes': {
+          'fill': item.fill
+        }
+      })
+    }
+  }
+})
+
+
 // support svg, png, PostScript(ps), Encapsulated PostScript(eps), pdf
-const command = 'M971.55 361.72H811.71V311.37H921.18A39.96 39.96 0 0 0 961.14 271.41V102.8A39.96 39.96 0 0 0 921.18 62.86H102.82A39.96 39.96 0 0 0 62.86 102.8V271.41A39.96 39.96 0 0 0 102.82 311.37H187.32V361.72H52.45A39.96 39.96 0 0 1 12.49 321.76V52.45A39.96 39.96 0 0 1 52.45 12.49H971.55A39.96 39.96 0 0 1 1011.51 52.45V321.76A39.96 39.96 0 0 1 971.55 361.72ZM686.88 697.42C686.88 718.95 669.07 736.43 647.09 736.43H377.31C355.35 736.43 337.52 718.95 337.52 697.42V611.58H387.15V650.59C387.15 672.12 404.95 687.25 426.93 687.25H596.44C618.42 687.25 636.23 669.8 636.23 648.24V537.35H387.27C359.8 537.35 337.52 515.05 337.52 487.52V312.37C337.52 284.85 359.8 262.57 387.27 262.57H637.15C664.63 262.57 686.88 284.85 686.88 312.37V487.52C686.88 496.34 685.28 504.51 682.43 511.7H686.88V697.42ZM636.88 337.17A24.98 24.98 0 0 0 611.9 312.2H412.1A24.98 24.98 0 0 0 387.12 337.17V462.05A24.98 24.98 0 0 0 412.1 487.02H611.9A24.98 24.98 0 0 0 636.88 462.05V337.17ZM137.14 922A39.96 39.96 0 0 0 177.23 961.91H846.52C868.68 961.91 886.63 944.05 886.63 922V362.15H936.29V971.55C936.29 993.63 918.45 1011.51 896.45 1011.51H127.55A39.89 39.89 0 0 1 87.71 971.55V362.15H137.14V922Z'
+const command = 'M970.48 514.88v130.43h-916.96v-130.43h891.69m39.39-55.18h-945.2c-21.69 0-39.37 17.7-39.37 39.39v162.01c0 21.69 17.68 39.4 39.37 39.4h945.2c21.72 0 39.37-17.7 39.37-39.4v-162.01c0-21.69-17.65-39.4-39.37-39.39z m-365.21-405.62v188.99h-214.78v-188.99h214.78m54.64-54.08h-324.06v296.56h324.06v-296.56z m-41.95 296.56l149.96 163.14h-540.48l150.61-163.14h239.91m41.95-53.49h-324.06l-249.54 271.81h825.29l-251.69-271.81z m54.57 780.9 a 19.67 19.67 0 0 1-19.7-19.67c0-35.52 12.77-54.65 23.02-70.01 9.14-13.74 16.38-24.58 16.38-48.15 0-23.47-7.19-34.2-16.27-47.78-10.29-15.36-23.12-34.47-23.13-70.36 a 19.67 19.67 0 1 1 39.4 0c0 23.9 7.25 34.74 16.44 48.45 10.21 15.25 22.93 34.22 22.93 69.69 0 35.52-12.75 54.62-22.98 70.01-9.16 13.72-16.38 24.58-16.39 48.15 a 19.67 19.67 0 0 1-19.7 19.67z m-157.53 0 a 19.7 19.7 0 0 1-19.7-19.67c0-35.52 12.77-54.65 23.01-70.01 9.16-13.74 16.38-24.58 16.39-48.15 0-23.47-7.19-34.2-16.28-47.78-10.29-15.36-23.12-34.47-23.12-70.36 a 19.7 19.7 0 0 1 39.4 0c0 23.9 7.25 34.74 16.44 48.45 10.21 15.25 22.93 34.22 22.93 69.69 0 35.52-12.75 54.62-22.99 70.01-9.16 13.72-16.38 24.58-16.38 48.15 a 19.67 19.67 0 0 1-19.7 19.67z m-157.54 0 a 19.67 19.67 0 0 1-19.67-19.67c0-35.52 12.75-54.65 22.99-70.01 9.16-13.74 16.38-24.58 16.38-48.15 0-23.47-7.19-34.2-16.27-47.78-10.29-15.36-23.09-34.47-23.1-70.36 a 19.67 19.67 0 1 1 39.37 0c0 23.9 7.25 34.74 16.44 48.45 10.21 15.25 22.96 34.22 22.96 69.69 0 35.52-12.77 54.62-23.01 70.01-9.16 13.72-16.38 24.58-16.39 48.15 a 19.67 19.67 0 0 1-19.7 19.67z m-157.53 0 a 19.67 19.67 0 0 1-19.67-19.67c0-35.52 12.75-54.65 22.98-70.01 9.16-13.74 16.38-24.58 16.39-48.15 0-23.47-7.19-34.2-16.28-47.78-10.29-15.36-23.09-34.47-23.09-70.36 a 19.67 19.67 0 1 1 39.37 0c0 23.9 7.25 34.74 16.44 48.45 10.21 15.25 22.96 34.22 22.96 69.69 0 35.52-12.77 54.62-23.02 70.01-9.16 13.72-16.38 24.58-16.38 48.15 a 19.67 19.67 0 0 1-19.7 19.67z'
 
-const pathDatas = pathParse(command).relCairo({
-  round: 2
-})
+// console.error('svgpath(command).unarc().round(2).toString()', svgpath(command).unarc().round(2).toString())
+// const pathData = pathParse(svgpath(command).unarc().round(2).toString()).absCairo({
+//   round: 2
+// })
 
-const pathDat = pathParse('M230 230A 45 45, 0, 1, 1, 275 275L 275 230 Z').absCairo({
-  round: 2
-})
-const { segments } = pathDatas
-// console.log('pathDatas', serializePath(pathDatas))
+// console.log('pathData', serializePath(pathData))
 // console.error('segments', segments)
 
 // fill: #ddd, stroke: red, stroke-width: 2, stroke-linecap: "butt", fill-rule:nonzero
-Parse('test.png', {
-  paths: [
-    {
-      d: segments,
-      attributes: {
-        fill: 'red',
-        stroke: '#000',
-        'stroke-width': 2,
-        'stroke-linecap': 'butt'
-      }
-    },
-    {
-      d: pathDat.segments,
-      attributes: {
-        fill: 'green',
-        stroke: '#fec',
-        'stroke-width': '10',
-        'stroke-linecap': 'round'
-      }
-    }
-  ],
+Parse('test222.eps', {
+  paths: pathDatas,
+  // paths: [
+  //   {
+  //     d: segments,
+  //     attributes: {
+  //       fill: '#666',
+  //       stroke: '#000',
+  //       // 'stroke-width': 2,
+  //       // 'stroke-linecap': 'butt'
+  //     }
+  //   }
+  //   // {
+  //   //   d: pathDat.segments,
+  //   //   attributes: {
+  //   //     fill: 'green',
+  //   //     stroke: '#fec',
+  //   //     'stroke-width': '10',
+  //   //     'stroke-linecap': 'round'
+  //   //   }
+  //   // }
+  // ],
   size: 1024
 }, (data, tips) => {
   console.error(data, tips)
